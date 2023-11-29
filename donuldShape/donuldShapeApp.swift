cd847684920f4e70f8628295295e227e6301bba4
itsmylifestyle <hahaii00@mail.ru>
2023-11-29 22:06:27 +0600

    Initial Commit

---
+++ donuldShape/donuldShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  donuldShapeApp.swift
+//  donuldShape
+//
+//  Created by Айбек on 29.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct donuldShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
